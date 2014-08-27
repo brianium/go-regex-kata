f079fd917e96080caa9812001141f67a773d8867
Brian Scaturro <scaturrob@gmail.com>
2014-08-27 12:27:33 -0400

    match offset

--- csvlex.go
+++ csvlex.go
@@ -10,7 +10,7 @@ const FieldSeparatorToken = 3
 const LineSeparatorToken = 4
 
 //internal map of patterns to tokens
-var tokens = map[string]int{
+var tokenMap = map[string]int{
 	`[^",\r\n]+`:                     PlainFieldToken,
 	`"[^"\\\\]*(?:\\\\.[^"\\\\]*)*"`: QuotedFieldToken,
 	`,`:        FieldSeparatorToken,
@@ -20,6 +20,7 @@ var tokens = map[string]int{
 type CsvLexer struct {
 	Src     []byte
 	Pattern *regexp.Regexp
+	offset  int
 }
 
 type CsvToken struct {
@@ -27,18 +28,29 @@ type CsvToken struct {
 	Value string
 }
 
-func patterns() []string {
-	ks := make([]string, 0, len(tokens))
-	for k := range tokens {
+func keys(mp map[string]int) []string {
+	ks := make([]string, 0, len(mp))
+	for k := range mp {
 		ks = append(ks, k)
 	}
 	return ks
 }
 
+func values(mp map[string]int) []int {
+	vals := make([]int, 0, len(mp))
+	for _, v := range mp {
+		vals = append(vals, v)
+	}
+	return vals
+}
+
+var patterns = keys(tokenMap)
+var tokens = values(tokenMap)
+
 func compile() *regexp.Regexp {
 	buffer := new(bytes.Buffer)
 	buffer.WriteString("((")
-	buffer.WriteString(strings.Join(patterns(), ")|("))
+	buffer.WriteString(strings.Join(patterns, ")|("))
 	buffer.WriteString("))")
 	return regexp.MustCompile(buffer.String())
 }
@@ -48,5 +60,7 @@ func New(src []byte) *CsvLexer {
 }
 
 func (l *CsvLexer) GetNext() *CsvToken {
-	return &CsvToken{Token: PlainFieldToken, Value: "One"}
+	subject := l.Src[l.offset:]
+	match := l.Pattern.FindSubmatch(subject)
+	return &CsvToken{Token: PlainFieldToken, Value: string(match[0])}
 }
